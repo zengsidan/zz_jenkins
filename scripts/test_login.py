07958b6376595c7a69dc2fb3f1bfdcc08a2ed3b8
zengsidan <375806999@qq.com>
2018-07-01 13:25:35 +0800

    初始化项目

---
+++ scripts/test_login.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class TestLogin:
+
+    def test_login1(self):
+        assert 1
+
+    def test_login2(self):
+        assert 1
+
+    def test_login3(self):
+        assert 1
+
+    def test_login4(self):
+        assert 1
+
+    def test_login5(self):
+        assert 0
+
+    def test_login6(self):
+        assert 0
